a42a930a1d39fdf2c071df926c45c8d49c93a18b
DennisKnol <dennisknol94@gmail.com>
2019-06-26 17:34:11 +0200

    WIP

--- ca_dennis.py
+++ ca_dennis.py
@@ -4,64 +4,61 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-class River:
-	"""
-	class following the Topa paper
+class CA:
 
-	"""
-	def __init__(self, size, n, p_branch=0.0, p_direct=0.5):
-		self.n = n
+	def __init__(
+			self, size, time_limit, slope, rand_lower=0.9999, rand_upper=1.00001,
+			branch_tresh=0.1, init_water=1, delta_water=0.001
+	):
 		self.size = size
+		self.time_limit = time_limit
+		self.branch_tresh = branch_tresh
+		self.init_water_level = init_water
 
-		# source cell, cell to which the water is supplied
-		self.starting_points = rd.sample(range(45, size - 45), n)
-
-		self.grid = np.zeros((size, size))
-
+		# starting point in the middle of the grid
+		self.starting_column = int(self.size / 2)
+		self.delta_w = delta_water
 		self.terrain = np.zeros((size, size))
-		self.water = np.zeros((size, size))
 		self.peat_bog = np.zeros((size, size))
 		self.nutrients = np.zeros((size, size))
+		self.path = np.zeros((size, size))
 
-		self.flow = 0
-
-		self.river_layer = 0
-		self.river_end = 0
-
-		self.p_branch = p_branch
-		self.p_direct = p_direct
-
-		self.mu = 0.01          # viscosity
-		self.gamma = 0.01       # gradient of nutrients concentration
-		self.rho = 0.01         # proportionality coefficient
-
-	def terrain_height(self):
-		"""
-		According to the paper, the terrain is slightly inclined (the slope is 0.05%) and rough
-		p. 1443: "we start from a flat and slanted landscape"
-		"""
-		terrain = self.terrain
-		terrain[0] = np.ones(100)
-
-		for i in range(self.size - 1):
-			terrain[i + 1] = terrain[i] * rd.uniform(0.999, 0.9999)
-
-		self.terrain = terrain
-		return self.terrain
-
-	def initiate_river(self):
-		"""
-		Initiate river in the middle of the grid
-		"""
-		river = np.zeros((100, 100))
-
-		for i in range(self.size):
-			river[i, 50] = 1
-
-		return river
+		self.rand_lower = rand_lower
+		self.rand_upper = rand_upper
+		self.river_coors = set()
+		self.segment_grid = {}
+		self.excluded_segments = {}
+		self.cur_river_nr = 1
+		self.segment_dict = {}
+		self.slope = slope
 
 	def moore_neighborhood(self, grid, i, j):
-		if not grid[i - 1, j]:
+
+		if i == 0 and j == 0:
+			neighborhood = [
+				grid[i + 1, j + 1],
+				grid[i, j + 1],
+				grid[i + 1, j],
+			]
+			locations = [
+				[i + 1, j + 1],
+				[i, j + 1],
+				[i + 1, j]
+			]
+
+		elif i == 0 and j == (self.size - 1):
+			neighborhood = [
+				grid[i, j - 1],
+				grid[i + 1, j - 1],
+				grid[i + 1, j],
+			]
+			locations = [
+				[i, j - 1],
+				[i + 1, j - 1],
+				[i + 1, j],
+			]
+
+		elif i == 0 and 0 < j < (self.size - 1):
 			neighborhood = [
 				grid[i, j - 1],
 				grid[i, j + 1],
@@ -69,8 +66,39 @@ class River:
 				grid[i + 1, j],
 				grid[i + 1, j + 1],
 			]
+			locations = [
+				[i, j - 1],
+				[i, j + 1],
+				[i + 1, j - 1],
+				[i + 1, j],
+				[i + 1, j + 1],
+			]
 
-		elif not grid[i + 1, j]:
+		elif i == (self.size - 1) and j == 0:
+			neighborhood = [
+				grid[i, j + 1],
+				grid[i - 1, j + 1],
+				grid[i - 1, j],
+			]
+			locations = [
+				[i, j + 1],
+				[i - 1, j + 1],
+				[i - 1, j],
+			]
+
+		elif i == (self.size - 1) and j == (self.size - 1):
+			neighborhood = [
+				grid[i - 1, j - 1],
+				grid[i, j - 1],
+				grid[i - 1, j],
+			]
+			locations = [
+				[i - 1, j - 1],
+				[i, j - 1],
+				[i - 1, j],
+			]
+
+		elif i == (self.size - 1) and 0 < j < (self.size - 1):
 			neighborhood = [
 				grid[i, j - 1],
 				grid[i, j + 1],
@@ -78,8 +106,15 @@ class River:
 				grid[i - 1, j],
 				grid[i - 1, j + 1],
 			]
+			locations = [
+				[i, j - 1],
+				[i, j + 1],
+				[i - 1, j - 1],
+				[i - 1, j],
+				[i - 1, j + 1],
+			]
 
-		elif not grid[i, j - 1]:
+		elif 0 < i < (self.size - 1) and j == 0:
 			neighborhood = [
 				grid[i - 1, j],
 				grid[i - 1, j + 1],
@@ -87,14 +122,28 @@ class River:
 				grid[i + 1, j],
 				grid[i + 1, j + 1],
 			]
+			locations = [
+				[i - 1, j],
+				[i - 1, j + 1],
+				[i, j + 1],
+				[i + 1, j],
+				[i + 1, j + 1],
+			]
 
-		elif not grid[i, j + 1]:
+		elif 0 < i < (self.size - 1) and j == (self.size - 1):
 			neighborhood = [
 				grid[i - 1, j],
 				grid[i - 1, j - 1],
 				grid[i, j - 1],
 				grid[i + 1, j],
 				grid[i + 1, j - 1],
+			]
+			locations = [
+				[i - 1, j],
+				[i - 1, j - 1],
+				[i, j - 1],
+				[i + 1, j],
+				[i + 1, j - 1],
 			]
 
 		else:
@@ -108,136 +157,199 @@ class River:
 				grid[i + 1, j],
 				grid[i + 1, j + 1],
 			]
-		return neighborhood
+			locations = [
+				[i - 1, j - 1],
+				[i - 1, j],
+				[i - 1, j + 1],
+				[i, j - 1],
+				[i, j + 1],
+				[i + 1, j - 1],
+				[i + 1, j],
+				[i + 1, j + 1],
+			]
 
-	def supply_water(self):
-		"""
-		add water to initial starting points and fill up at each iteration
+		return neighborhood, locations
 
-		p.1442: "Water is supplied to the system by a single source cell."
+	def get_location_of_lowest_neighbor(self, grid, i, j, river_nr):
+		neighborhood = self.moore_neighborhood(grid, i, j)
+		neighborhood0, neighborhood1 = [], []
+		for i, val in enumerate(neighborhood[1]):
+			val_tup = tuple(val)
+			if val_tup in self.segment_grid:
+				if self.segment_grid[val_tup] not in self.excluded_segments[river_nr]:
+					neighborhood0.append(neighborhood[0][i])
+					neighborhood1.append(val)
+			else:
+				neighborhood0.append(neighborhood[0][i])
+				neighborhood1.append(val)
+		try:
+			value, location = (list(t) for t in zip(*sorted(zip(neighborhood0, neighborhood1))))
+		except ValueError:
+			value, location = [], []
 
-		p.906: "Water is supplied to the systems by ,,source” cells,
-		in which the amount of water is filled up in each iteration."
-		"""
+		return value, location
 
-		pass
+	def get_path(self, prev_val, coor_list, value_list, prev_coor):
+		for i, coor in enumerate(coor_list):
+			tup = tuple(coor)
 
-	def remove_water(self):
-		"""
-		remove water from the outlet
-		"""
+			if tup not in self.river_coors:
+				self.river_coors.add(tup)
+				if not self.segment_grid and len(coor_list) == 1:
+					self.excluded_segments[self.cur_river_nr] = [self.cur_river_nr]
+					self.segment_grid[tup] = self.cur_river_nr
+					self.segment_dict[self.cur_river_nr] = [tup]
+					self.cur_river_nr += 1
+				elif len(coor_list) > 1:
+					# print('splitsing', coor_list, self.cur_river_nr)
+					self.excluded_segments[self.cur_river_nr] = [self.cur_river_nr]
+					self.segment_grid[tup] = self.cur_river_nr
+					self.segment_dict[self.cur_river_nr] = [tup]
+					self.cur_river_nr += 1
+				else:
+					self.segment_dict[self.segment_grid[prev_coor]].append(tup)
+					self.segment_grid[tup] = self.segment_grid[prev_coor]
 
-		pass
+			else:
+				self.excluded_segments[self.cur_river_nr] = [
+					self.cur_river_nr, self.segment_grid[prev_coor], self.segment_grid[tup]
+				]
+				self.update_segment(tup, self.segment_grid[tup], self.cur_river_nr)
+				self.segment_grid[tup] = self.cur_river_nr
+				self.segment_dict[self.cur_river_nr] = [tup]
+				self.cur_river_nr += 1
 
-	def calculate_flow(self):
+			self.path[tup] = self.path[tup] + float(prev_val[i]) * (1 - self.delta_w)
+		return self.path
 
-		return self.flow
+	def update_segment(self, coor, old_nr, new_nr):
+		# print(coor)
+		# print(self.segment_dict)
+		# print(self.segment_grid)
+		coor_idx = self.segment_dict[old_nr].index(coor)
+		self.segment_dict[new_nr] = self.segment_dict[old_nr][coor_idx:]
+		self.segment_dict[old_nr] = self.segment_dict[old_nr][:coor_idx]
+		for val in self.segment_dict[new_nr]:
+			self.segment_grid[val] = new_nr
+		return self.segment_dict
 
-	def update_water(self):
+	def create_path_from_start(self):
+
+		self.path = self.get_path(
+			[self.init_water_level / (1 - self.delta_w)], [(0, self.starting_column)],
+			[self.init_water_level], [(0, self.starting_column)]
+		)
+
+		self.river_coors.add((0, self.starting_column))
+		cur_ends = set()
+		cur_ends.add((0, self.starting_column))
+
+		for x in range(1, self.time_limit):
+			temp_ends = set()
+			for i, item in enumerate(cur_ends):
+				# print(item)
+				if self.path[item] > self.branch_tresh:
+					old_value = self.terrain[item]
+					sort_values, sort_location = self.get_location_of_lowest_neighbor(
+						self.terrain, item[0], item[1], self.segment_grid[item]
+					)
+					if not sort_values:
+						continue
+
+					next_cell, next_value = [tuple(sort_location[0])], [sort_values[0]]
+					temp_ends.add(next_cell[0])
+					next_water = [self.path[item]]
+					if old_value < sort_values[0] and len(sort_location) > 1:
+						next_cell.append(tuple(sort_location[1]))
+						next_value.append(sort_values[1])
+						next_water = self.new_water_ratio(item, tuple(sort_location[0]), tuple(sort_location[1]))
+
+						temp_ends.add(next_cell[1])
+
+						# SAVE AFTER SPLITTING LOCATION
+						first_leg = sort_location[0]
+						second_leg = sort_location[1]
+
+						print("hier", i, first_leg)
+						print(i, second_leg)
+
+					self.path = self.get_path(next_water, next_cell, next_value, item)
+
+			cur_ends = temp_ends.copy()
+			if not cur_ends:
+				return self.path
+
+		return self.path
+
+	def new_water_ratio(self, old, coor_split1, coor_split2):
+		new_l = self.terrain[coor_split1] - self.terrain[old]
+		new_r = self.terrain[coor_split2] - self.terrain[old]
+		l = new_l / (new_l + new_r) * self.path[old]
+		r = new_r / (new_l + new_r) * self.path[old]
+		return [l, r]
+
+	def initialize_terrain(self):
+		terrain = np.ones((self.size, self.size))
 
 		for i in range(self.size):
 			for j in range(self.size):
-				self.water = self.water + self.flow
+				neighbors = self.moore_neighborhood(terrain, i, j)[0]
+				if rd.random() < 0.01:
+					perturb = rd.uniform(0.999, 1.0001)
+				else:
+					perturb = rd.uniform(self.rand_lower, self.rand_upper)
+				terrain[i, j] = np.mean(neighbors) * perturb
 
-		return self.water
+		# create hill top coordinates
+		hill_coordinates = [
+			(int(self.size / 8), int(self.size / 1.2)),
+			(int(self.size / 3), int(self.size / 5)),
+			(int(self.size / 1.6), int(self.size / 1.4)),
+		]
+		for hill_coords in hill_coordinates:
+			terrain[hill_coords] = terrain[hill_coords] # * 1
 
-	def distribute_nutrients(self):
+		for _ in range(5):
+
+			for i in range(self.size):
+				for j in range(self.size):
+					neighborhood, locations = self.moore_neighborhood(terrain, i, j)
+					for n, neighbor in enumerate(neighborhood):
+						location = (locations[n][0], locations[n][1])
+						if ((terrain[i, j] - neighbor) / neighbor) > 0.01:
+							terrain[location] = terrain[i, j] * rd.uniform(0.995, 0.999)
+
+			for i in range(self.size - 1, 0, -1):
+				for j in range(self.size - 1, 0, -1):
+					neighborhood, locations = self.moore_neighborhood(terrain, i, j)
+					for n, neighbor in enumerate(neighborhood):
+						location = (locations[n][0], locations[n][1])
+						if ((terrain[i, j] - neighbor) / neighbor) > 0.01:
+							terrain[location] = terrain[i, j] * rd.uniform(0.995, 0.999)
 
 		for i in range(self.size):
-			for j in range(self.size):
-				nutrients_neighbors = self.moore_neighborhood(self.nutrients, i, j)
-				if self.water[i, j] > 0:
-					self.nutrients[i, j] = 1.0
-				elif self.nutrients[i, j] < max(nutrients_neighbors):
-					self.nutrients = self.gamma * max(nutrients_neighbors)
+			terrain[i] = terrain[i] * (1 - self.slope * i)
 
-		return self.nutrients
-
-	def update_peatbog(self):
-
-		for i in range(self.size):
-			for j in range(self.size):
-				if self.water[i, j] == 0:
-					self.peat_bog[i, j] = self.peat_bog[i, j] + self.rho * self.nutrients[i, j]
-
-		return self.peat_bog
-
-	def update_terrain(self):
-
-		for i in range(self.size):
-			for j in range(self.size):
-				if self.water[i, j] > 0:
-					self.terrain[i, j] = self.terrain[i, j] + self.mu * self.water[i, j]
+		self.terrain = terrain
 
 		return self.terrain
 
-	def generate_river(self):
-		self.river_layer += 1
-
-		new_river_end = []
-		for val in self.river_end:
-			if val[1] == self.size - 1:
-				direct = rd.randint(-1, 0)
-			elif val[1] == 0:
-				direct = rd.randint(0, 1)
-			else:
-				direct = rd.randint(-1, 1)
-			x, y = self.river_layer, (val[1] + direct)
-			self.grid[(x, y)] = 1
-			new_river_end.append((x, y))
-		self.river_end = new_river_end
-		return new_river_end
-
-	def water_height(self):
-		"""
-		Similar approach as above, but water height increases
-		"""
-		water = self.water
-		water[0] = np.ones(100) * rd.uniform(0.08, 0.12)
-
-		for i in range(self.size - 1):
-			for j in range(self.size - 1):
-				if water[i, j + 1] and water[i, j - 1]:
-					mean = (water[i, j - 1] + water[i, j] + water[i, j + 1]) / 3
-					water[i + 1, j] = mean * rd.uniform(1.001, 1.005)
-				if water[i, j + 1] and not water[i, j - 1]:
-					mean = (water[i, j] + water[i, j + 1]) / 2
-					water[i + 1, j] = mean * rd.uniform(1.001, 1.005)
-				if water[i, j - 1] and not water[i, j + 1]:
-					mean = (water[i, j] + water[i, j - 1]) / 2
-					water[i + 1, j] = mean * rd.uniform(1.001, 1.005)
-
-		self.water = water
-		return self.water
-
-	def total_height(self):
-		"""
-		Sum of the terrain height and water height
-		"""
-		total_height = np.zeros((self.size, self.size))
-
-		for i in range(self.size):
-			for j in range(self.size):
-				total_height[i, j] = self.terrain[i, j] + self.water[i, j]
-
-		return total_height
-
 
 if __name__ == "__main__":
-	rv = River(100, 1)
-	rv.calculate_flow()
-	for i in range(99):
-		rv.generate_river()
+	for i in range(1):
+		ca = CA(size=500, time_limit=500, slope=0.0005)
+		terrain = ca.initialize_terrain()
+		path = ca.create_path_from_start()
+		np.savetxt(f'tests/test_final.csv', path, delimiter=',')
 
-	terrain = rv.terrain_height()
-	water = rv.water_height()
+		plt.figure(figsize=(15, 5))
 
-	fig, axes = plt.subplots(1, 3)
-	sns.heatmap(terrain[:, 0:99], cmap="BrBG_r", vmin=0.85, vmax=1.005, ax=axes[0])
-	sns.heatmap(water[:, 0:99], cmap="Blues", ax=axes[1])
-	plt.show()
+		plt.subplot2grid((1, 2), (0, 0))
+		sns.heatmap(terrain[:, 0:99], cmap="Greens")
+		plt.title("Terrain with slope")
 
-
-
-	# plt.imshow(rv.grid)
-	# plt.show()
+		plt.subplot2grid((1, 2), (0, 1))
+		sns.heatmap(path, cmap="Blues")
+		plt.title("River")
+		# plt.savefig(f'plots/river_{i}.png', dpi=300)
+		plt.show()
---
+++ ca_hills.py
@@ -0,0 +1,368 @@
+import numpy as np
+import random as rd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+class CA:
+	"""
+	NOTES:
+		According to Topa the complexity of the river system is measured by the number of
+		channels and bifurcations
+
+		”Anastomosing river” term refers to river system that possess extremely complex
+		network of forking and joining channels
+
+		The new channels usually merge with the others, creating a complex network composed
+		of splitting and merging water channels and small lakes
+
+	"""
+
+	def __init__(
+			self, size, slope, mu, gamma, rho, time_limit, rand_lower=0.9999, rand_upper=1.00001,
+			branch_tresh=0.1, init_water=1, delta_water=0.001
+	):
+		self.size = size
+		self.slope = slope
+		self.time_limit = time_limit
+		self.branch_tresh = branch_tresh
+		self.init_water_level = init_water
+		# starting point in the middle of the grid
+		self.starting_column = int(self.size / 2)
+		self.delta_w = delta_water
+		self.terrain = np.zeros((size, size))
+		self.peat_bog = np.zeros((size, size))
+		self.nutrients = np.zeros((size, size))
+		self.cur_river_nr = 0
+		self.rivers = {}
+
+		self.path = np.zeros((size, size))
+
+		self.mu = mu  # viscosity
+		self.gamma = gamma  # gradient of nutrients concentration
+		self.rho = rho  # proportionality coefficient
+		self.rand_lower = rand_lower
+		self.rand_upper = rand_upper
+		self.river_coors = set()
+
+	def moore_neighborhood(self, grid, i, j):
+
+		if i == 0 and j == 0:
+			neighborhood = [
+				grid[i + 1, j + 1],
+				grid[i, j + 1],
+				grid[i + 1, j],
+			]
+			locations = [
+				[i + 1, j + 1],
+				[i, j + 1],
+				[i + 1, j]
+			]
+
+		elif i == 0 and j == (self.size - 1):
+			neighborhood = [
+				grid[i, j - 1],
+				grid[i + 1, j - 1],
+				grid[i + 1, j],
+			]
+			locations = [
+				[i, j - 1],
+				[i + 1, j - 1],
+				[i + 1, j],
+			]
+
+		elif i == 0 and 0 < j < (self.size - 1):
+			neighborhood = [
+				grid[i, j - 1],
+				grid[i, j + 1],
+				grid[i + 1, j - 1],
+				grid[i + 1, j],
+				grid[i + 1, j + 1],
+			]
+			locations = [
+				[i, j - 1],
+				[i, j + 1],
+				[i + 1, j - 1],
+				[i + 1, j],
+				[i + 1, j + 1],
+			]
+
+		elif i == (self.size - 1) and j == 0:
+			neighborhood = [
+				grid[i, j + 1],
+				grid[i - 1, j + 1],
+				grid[i - 1, j],
+			]
+			locations = [
+				[i, j + 1],
+				[i - 1, j + 1],
+				[i - 1, j],
+			]
+
+		elif i == (self.size - 1) and j == (self.size - 1):
+			neighborhood = [
+				grid[i - 1, j - 1],
+				grid[i, j - 1],
+				grid[i - 1, j],
+			]
+			locations = [
+				[i - 1, j - 1],
+				[i, j - 1],
+				[i - 1, j],
+			]
+
+		elif i == (self.size - 1) and 0 < j < (self.size - 1):
+			neighborhood = [
+				grid[i, j - 1],
+				grid[i, j + 1],
+				grid[i - 1, j - 1],
+				grid[i - 1, j],
+				grid[i - 1, j + 1],
+			]
+			locations = [
+				[i, j - 1],
+				[i, j + 1],
+				[i - 1, j - 1],
+				[i - 1, j],
+				[i - 1, j + 1],
+			]
+
+		elif 0 < i < (self.size - 1) and j == 0:
+			neighborhood = [
+				grid[i - 1, j],
+				grid[i - 1, j + 1],
+				grid[i, j + 1],
+				grid[i + 1, j],
+				grid[i + 1, j + 1],
+			]
+			locations = [
+				[i - 1, j],
+				[i - 1, j + 1],
+				[i, j + 1],
+				[i + 1, j],
+				[i + 1, j + 1],
+			]
+
+		elif 0 < i < (self.size - 1) and j == (self.size - 1):
+			neighborhood = [
+				grid[i - 1, j],
+				grid[i - 1, j - 1],
+				grid[i, j - 1],
+				grid[i + 1, j],
+				grid[i + 1, j - 1],
+			]
+			locations = [
+				[i - 1, j],
+				[i - 1, j - 1],
+				[i, j - 1],
+				[i + 1, j],
+				[i + 1, j - 1],
+			]
+
+		else:
+			neighborhood = [
+				grid[i - 1, j - 1],
+				grid[i - 1, j],
+				grid[i - 1, j + 1],
+				grid[i, j - 1],
+				grid[i, j + 1],
+				grid[i + 1, j - 1],
+				grid[i + 1, j],
+				grid[i + 1, j + 1],
+			]
+			locations = [
+				[i - 1, j - 1],
+				[i - 1, j],
+				[i - 1, j + 1],
+				[i, j - 1],
+				[i, j + 1],
+				[i + 1, j - 1],
+				[i + 1, j],
+				[i + 1, j + 1],
+			]
+
+		return neighborhood, locations
+
+	def initialize_terrain(self):
+		"""
+		NEW: added "hills" to the terrain
+		"""
+		terrain = np.ones((self.size, self.size))
+
+		for i in range(self.size):
+			for j in range(self.size):
+				neighbors = self.moore_neighborhood(terrain, i, j)[0]
+				if rd.random() < 0.01:
+					perturb = rd.uniform(0.999, 1.0001)
+				else:
+					perturb = rd.uniform(self.rand_lower, self.rand_upper)
+				terrain[i, j] = np.mean(neighbors) * perturb
+
+		# create hill top coordinates
+		hill_coordinates = [
+			(int(self.size / 8), int(self.size / 1.2)),
+			(int(self.size / 3), int(self.size / 5)),
+			(int(self.size / 1.6), int(self.size / 1.4)),
+		]
+		for hill_coords in hill_coordinates:
+			terrain[hill_coords] = terrain[hill_coords] * 1.2
+
+		for _ in range(5):
+
+			for i in range(self.size):
+				for j in range(self.size):
+					neighborhood, locations = self.moore_neighborhood(terrain, i, j)
+					for n, neighbor in enumerate(neighborhood):
+						location = (locations[n][0], locations[n][1])
+						if ((terrain[i, j] - neighbor)/neighbor) > 0.01:
+							terrain[location] = terrain[i, j] * rd.uniform(0.995, 0.999)
+
+			for i in range(self.size-1, 0, -1):
+				for j in range(self.size-1, 0, -1):
+					neighborhood, locations = self.moore_neighborhood(terrain, i, j)
+					for n, neighbor in enumerate(neighborhood):
+						location = (locations[n][0], locations[n][1])
+						if ((terrain[i, j] - neighbor) / neighbor) > 0.01:
+							terrain[location] = terrain[i, j] * rd.uniform(0.995, 0.999)
+
+			for i in range(self.size):
+				for j in range(self.size-1, 0, -1):
+					neighborhood, locations = self.moore_neighborhood(terrain, i, j)
+					for n, neighbor in enumerate(neighborhood):
+						location = (locations[n][0], locations[n][1])
+						if ((terrain[i, j] - neighbor) / neighbor) > 0.01:
+							terrain[location] = terrain[i, j] * rd.uniform(0.995, 0.999)
+
+			for i in range(self.size-1, 0, -1):
+				for j in range(self.size):
+					neighborhood, locations = self.moore_neighborhood(terrain, i, j)
+					for n, neighbor in enumerate(neighborhood):
+						location = (locations[n][0], locations[n][1])
+						if ((terrain[i, j] - neighbor) / neighbor) > 0.01:
+							terrain[location] = terrain[i, j] * rd.uniform(0.995, 0.999)
+
+
+		for i in range(self.size):
+			terrain[i] = terrain[i] * (1 - self.slope * i)
+
+		self.terrain = terrain
+
+		return self.terrain
+
+	def get_location_of_lowest_neighbor(self, grid, i, j, temp_ends):
+		neighborhood = self.moore_neighborhood(grid, i, j)
+		neighborhood0, neighborhood1 = [], []
+		for i, val in enumerate(neighborhood[1]):
+			if tuple(val) not in self.river_coors:
+				# and tuple(val) not in temp_ends
+				neighborhood0.append(neighborhood[0][i])
+				neighborhood1.append(val)
+		try:
+			value, location = (list(t) for t in zip(*sorted(zip(neighborhood0, neighborhood1))))
+		except ValueError:
+			value, location = [], []
+
+		return value, location
+
+	def get_path(self, prev_val, coor_list, value_list):
+		for i, coor in enumerate(coor_list):
+			tup = tuple(coor)
+			# if self.path[tup] > self.branch_tresh:
+			if tup not in self.river_coors:
+				self.river_coors.add(tup)
+			# self.path[tup] = prev_val[i]*(1-self.delta_w)
+
+			else:
+				pass
+			# print('jup', self.path[tup], value_list[i])
+			self.path[tup] = self.path[tup] + float(prev_val[i]) * (1 - self.delta_w)
+		# self.path[tup] = self.path[tup] + prev_val[i]*(1-self.delta_w)
+		return self.path
+
+	def create_path_from_start(self):
+		self.path = self.get_path([self.init_water_level / (1 - self.delta_w)], [(0, 50)], [self.init_water_level])
+
+		# next_cell = sort_location[0]
+		self.river_coors.add((0, self.starting_column))
+		cur_ends = set()
+		cur_ends.add((0, self.starting_column))
+
+		for x in range(1, self.time_limit):
+			temp_ends = set()
+			for i, item in enumerate(cur_ends):
+				if self.path[item] > self.branch_tresh:
+					old_value = self.terrain[item]
+					sort_values, sort_location = self.get_location_of_lowest_neighbor(self.terrain, item[0], item[1], temp_ends)
+					if not sort_values:
+						continue
+					next_cell, next_value = [tuple(sort_location[0])], [sort_values[0]]
+					temp_ends.add(next_cell[0])
+					next_water = [self.path[item]]
+					# print(old_value, sort_values, sort_location)
+					if old_value < sort_values[0] and len(sort_location) > 1:
+						next_cell.append(tuple(sort_location[1]))
+						next_value.append(sort_values[1])
+
+						next_water = self.new_water_ratio(item, tuple(sort_location[0]), tuple(sort_location[1]))
+
+						temp_ends.add(next_cell[1])
+					self.path = self.get_path(next_water, next_cell, next_value)
+					try:
+						print(self.path[tuple(sort_location[0])], self.path[tuple(sort_location[1])])
+					except:
+						pass
+			cur_ends = temp_ends.copy()
+			if not cur_ends:
+				return self.path
+
+		return self.path
+
+	def new_water_ratio(self, old, coor_split1, coor_split2):
+		new_l = self.terrain[coor_split1] - self.terrain[old]
+		new_r = self.terrain[coor_split2] - self.terrain[old]
+		l = new_l / (new_l + new_r) * self.path[old]
+		r = new_r / (new_l + new_r) * self.path[old]
+		return [l, r]
+
+	def calculate_nutrient_distribution(self):
+
+		for i in range(self.size):
+			for j in range(self.size):
+				if self.water[i, j] > 0:
+					self.nutrients[i, j] = 1
+				else:
+					neighborhood = self.moore_neighborhood(self.nutrients[i, j])
+					max_value = max(neighborhood)
+					self.nutrients[i, j] = self.gamma * max_value
+
+		return self.nutrients
+
+	def calculate_peat_growth(self):
+
+		for i in range(self.size):
+			for j in range(self.size):
+				if self.water[i, j] > 0:
+					self.peat_bog[i, j] = self.mu * self.nutrients[i, j]
+				else:
+					self.peat_bog[i, j] = self.rho * self.nutrients[i, j]
+
+		return self.peat_bog
+
+	# def width_length_ratio(self):
+	#
+	# 	for i in range(self.size):
+
+
+if __name__ == "__main__":
+	for i in range(1):
+		for slope in [0.0005, 0.001]: #0.001, 0.002
+			ca = CA(size=100, slope=slope, mu=0.0004, gamma=0.0002, rho=0.02, time_limit=100)
+			terrain = ca.initialize_terrain()
+			path = ca.create_path_from_start()
+			np.savetxt(f'tests/test_final.csv', path, delimiter=',')
+			fig, axes = plt.subplots(1, 2)
+			sns.heatmap(terrain[:, 0:99], cmap="Greens", ax=axes[0])
+			sns.heatmap(path, cmap="Blues", ax=axes[1])
+			# axes[1].set_title("Path of river without bifurcation")
+			# plt.savefig(f'plots/river_{i}.png', dpi=300)
+			plt.show()
